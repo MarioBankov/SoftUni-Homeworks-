83ee221915317b9c0551fe21db3ea3205d5dbd59
MarioBankov <mariobankov@mail.bg>
2019-08-23 00:35:49 +0300

    Advanced Exam

---
+++ Advanced Retake Exam - 16 April 2019/01. Trojan Invasion/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Trojan_Invasion
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> platesList = Console.ReadLine().Split().Select(int.Parse).ToList();
+            Queue<int> platesQueue= new Queue<int>(platesList);
+            Stack<int> warriorsStack = new Stack<int>();
+            for (int i = 1; i <= n; i++)
+            {
+                List<int> warriorsList = Console.ReadLine().Split().Select(int.Parse).ToList();
+                AddWarriors(warriorsStack, warriorsList);
+                if (i%3==0)
+                {
+                    List<int> additionalPlate = Console.ReadLine().Split().Select(int.Parse).ToList();
+                    for (int j = 0; j < additionalPlate.Count; j++)
+                    {
+                        platesList.Add(additionalPlate[j]);
+                    }
+                }
+                while (warriorsStack.Count>0&&platesList.Count>0)
+                {
+                    int currentWarrior = warriorsStack.Pop();
+                    int currentPlate = platesList[0];
+                    if(currentWarrior>currentPlate)
+                    {
+                        currentWarrior -= currentPlate;
+                        warriorsStack.Push(currentWarrior);
+                        platesList.RemoveAt(0);
+                    }
+                    else if (currentWarrior<currentPlate)
+                    {
+                        currentPlate -= currentWarrior;
+                        platesList[0] = currentPlate;
+                    }
+                    else
+                    {
+                        platesList.RemoveAt(0);
+                    }
+                }
+                if (platesList.Count==0)
+                {
+                    break;
+                }
+              
+            }
+            if (platesList.Count == 0)
+            {
+                Console.WriteLine("The Trojans successfully destroyed the Spartan defense.");
+                Console.WriteLine("Warriors left: {0}", string.Join(", ", warriorsStack));
+            }
+            else
+            {
+                Console.WriteLine("The Spartans successfully repulsed the Trojan attack.");
+                Console.WriteLine("Plates left: {0}",string.Join(", ",platesList));
+            }
+
+        }
+
+        private static void AddWarriors(Stack<int> warriorsStack, List<int> warriorsList)
+        {
+            foreach (var item in warriorsList)
+            {
+                warriorsStack.Push(item);
+            }
+        }
+    }
+}
---
+++ Advanced Retake Exam - 16 April 2019/Helen's Abduction/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace Helen_s_Abduction
+{
+    class Program
+    {
+        static char[][] field = null;
+        static int playerRow;
+        static int playerCol;
+        static void Main(string[] args)
+        {
+            int energy = int.Parse(Console.ReadLine());
+            int rows = int.Parse(Console.ReadLine());
+            field = new char[rows][];
+            for (int i = 0; i < rows; i++)
+            {
+                field[i] = Console.ReadLine().ToCharArray();
+                for (int j = 0; j < field[i].Length; j++)
+                {
+                    if (field[i][j] == 'P')
+                    {
+                        playerRow = i;
+                        playerCol = j;
+                    }
+                }
+            }
+            field[playerRow][playerCol] = '-';
+            bool win = false;
+            while (energy > 0)
+            {
+                string[] command = Console.ReadLine().Split();
+                string direction = command[0];
+                int spawnRow = int.Parse(command[1]);
+                int spawnCol = int.Parse(command[2]);
+
+                SpawnEnemy(spawnRow, spawnCol);
+                field[playerRow][playerCol] = '-';
+                MovePlayer(direction);
+                
+
+                energy--;
+                if (field[playerRow][playerCol] == 'S')
+                {
+                    energy -= 2;
+                    field[playerRow][playerCol] = 'P';
+                }
+                else if (field[playerRow][playerCol] == 'H')
+                {
+                    field[playerRow][playerCol] = '-';
+                    win = true;
+                    break;
+                }
+                else if (field[playerRow][playerCol]=='-')
+                {
+                    field[playerRow][playerCol] = 'P';
+                }
+                if (energy<=0)
+                {
+                    field[playerRow][playerCol] = 'X';
+                    break;
+                }
+
+                
+            }
+
+            if (win)
+            {
+                Console.WriteLine($"Paris has successfully abducted Helen! Energy left: {energy}");
+
+            }
+            else
+            {
+                Console.WriteLine($"Paris died at {playerRow};{playerCol}.");
+            }
+            for (int i = 0; i < rows; i++)
+            {
+                Console.WriteLine(string.Join("", field[i]));
+            }
+        }
+
+        private static void MovePlayer(string direction)
+        {
+            if (direction == "up")
+            {
+                if (playerRow - 1 >= 0)
+                {
+                    playerRow--;
+                }
+            }
+            else if (direction == "down")
+            {
+                if (playerRow + 1 < field.Length)
+                {
+                    playerRow++;
+                }
+            }
+            else if (direction == "right")
+            {
+                if (playerCol + 1 < field[playerRow].Length)
+                {
+                    playerCol++;
+                }
+            }
+            else if (direction == "left")
+            {
+                if (playerCol - 1 >= 0)
+                {
+                    playerCol--;
+                }
+            }
+        }
+
+        private static void SpawnEnemy(int x, int y)
+        {
+            field[x][y] = 'S';
+        }
+
+
+    }
+}
