2aa3aac3c0ef8320b8e73edb4b34b796ab028d6a
MarioBankov <mariobankov@mail.bg>
2019-08-27 03:18:54 +0300

    Lab: Inheritance

---
+++ Lab  Inheritance/Random List/RandomList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class RandomList:List<string>
+    {
+        private Random rng = new Random();
+        public string RandomString()
+        {
+            var lenght = this.Count;
+            var index  = rng.Next(0, lenght);
+            var element = this[index];
+            this.RemoveAt(index);
+            return element;
+        }
+    }
+}
---
+++ Lab  Inheritance/Random List/StackOfStrings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return Count > 0;
+        }
+        public void AddRange(Stack<string> stack)
+        {
+            foreach (var item in stack)
+            {
+                Push(item);
+            }
+        }
+    }
+}
